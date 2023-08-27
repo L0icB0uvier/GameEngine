442229f5efe3dbc7e3019c7a3750b12b49674944
Loïc Bouvier <loic.bouvier80@gmail.com>
2023-08-27 18:05:21 +0200

    Project setup

---
+++ GameEngine/src/GameEngine.h
@@ -0,0 +1,8 @@
+#pragma once
+
+//For use by Game Engine applications
+#include "GameEngine/Application.h"
+
+// ---------- Entry Point -----------
+#include "GameEngine/EntryPoint.h"
+// ----------------------------------
---
+++ GameEngine/src/GameEngine/Core.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifdef GE_PLATFORM_WINDOWS
+	#ifdef GE_BUILD_DLL
+		#define GAME_ENGINE_API __declspec(dllexport)
+	#else
+		#define GAME_ENGINE_API __declspec(dllimport)
+	#endif // GE_BUILD_DLL
+#else
+	#error Game Engine only support Windows!
+
+#endif // GE_PLATFORM_WINDOWS
